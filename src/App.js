4ba0f215ed46b8f7cc0366fd4436b9c8e9ee79b6
gulshant <gulshant113@gmail.com>
2023-08-10 15:33:35 +0530

    Update With New Registration Form Without CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
-import LoginPage from "./dynamic-forms/LoginPage";
+// import LoginPage from "./dynamic-forms/LoginPage";
+import RegistrationForm from "./dynamic-forms/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <RegistrationForm />
     </div>
   );
 }
